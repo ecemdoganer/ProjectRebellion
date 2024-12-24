65656cce2851353bdcf392f7734573012d5f76c5
“ecemdoganer” <doganerecem35@hotmail.com>
2024-12-24 15:29:07 +0300

    Player selection
    Added a player and scripts.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,10 +13,17 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-        {
+        // Flip player when moving left and right
+        if (horizontalInput > 0.01f) {
+            transform.localScale = new Vector3((float)-4.0961, (float)4.007428, (float)0.5); 
+        } else if (horizontalInput < -0.01f) {
+            transform.localScale = new Vector3((float)4.0961, (float)4.007428, (float)0.5);
+        }
+        
+        if (Input.GetKey(KeyCode.Space)) {
             body.velocity = new Vector2(body.velocity.x, speed);
         }
         
